81be23b72a656f1dd490f16e63d1f3b8433462dd
khoa0319 <khoa0319@gmail.com>
2019-04-13 16:52:55 +0700

    create users route

--- server/index.js
+++ server/index.js
@@ -10,8 +10,9 @@ const connection = mysql.createConnection({ host, user, password, database });
 connection.connect((err) => {
   if (err) return console.log(err);
   console.log('Connected as id', connection.threadId);
-})
+});
 
+app.use('/api/users', require('./routes/api/users'));
 const port = config.port || 5000;
 
 app.listen(port, () => {
---
+++ server/routes/api/users.js
@@ -0,0 +1,13 @@
+/* 3rd party modules */
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({
+    'message': `hello ${req.params['id']}`
+  });
+  res.end();
+})
+
+module.exports = router;
