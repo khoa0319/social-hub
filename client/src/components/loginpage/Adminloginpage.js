bea2f4287066fa0cf65a6f5bd20662d54172bba5
unknown <hungjkl1@gmail.com>
2019-04-17 14:05:04 +0700

    New Interface User

---
+++ client/src/components/loginpage/Adminloginpage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Adminloginpage extends Component {
+    render() {
+        return (
+            <div className="row">
+            <div class="col-md-12">
+                <div class="card mb-4 f-elm text-center ">
+                     <div class="card-header bg-main text-light align-middle">
+                       <h4>Đăng Nhập vào trang Admin</h4>
+                     </div>
+                     <div class="card-body">
+                       <div class="form-group">
+                       <h6><label>Tên Đăng Nhập</label></h6>
+                         <input type="" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="Username"/>
+                         <h6><label>Mật Khẩu</label> </h6>
+                         <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="Password"/>
+                         <p>
+                         <div className="mt-2">>
+                         <button type="button" class="btn btn-myapp  col-12 col-md-12 col-lg-12">Đăng Nhập</button>
+                         </div>
+                        </p>
+                     </div>
+                   </div>
+                  </div>
+                  </div>
+                  </div>
+        );
+    }
+}
+
+export default Adminloginpage;
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import NavbarSmall from './NavbarSmall'
+import NavbarBig from './NavbarBig'
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+               
+                <div className="d-block d-sm-none">
+        
+          <img src='img/logo.png' className="logo d-flex justify-content-center mx-auto d-block" />
+
+
+                <NavbarSmall/>
+                </div>
+                <div className="container d-none d-sm-block">
+                <NavbarBig/>
+                </div>
+                 
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
