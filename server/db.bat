80d6d99afe3be06a8b9efc7ccd39ad3e94913a38
unknown <khoa0319@gmail.com>
2019-04-21 08:51:44 +0700

    hot fix bw window and mac create db

---
+++ server/db.bat
@@ -0,0 +1,2 @@
+@echo off
+mysql -uroot -pkhoa0319 -hlocalhost  -e "source D:\Projects\Gits\social-hub\server\models\socialhub.sql"
--- server/spawnDatabase.js
+++ server/spawnDatabase.js
@@ -1,28 +1,25 @@
 const { spawn } = require('child_process');
-const os = require('os');
-if (os.platform() === 'darwin') {
-  const database = {
-    user: 'root',
-    password: 'khoa0319',
-    address: 'localhost',
-    file: './models/socialhub.sql'
-  }
-  
-  let mysqlImport = spawn('mysql', [
-    '-u' + database.user,
-    '-p' + database.password,
-    '-h' + database.address]);
-  mysqlImport.stdin.write('source ' + database.file);
-  mysqlImport.stdin.end();
-  
-  mysqlImport.stdout
-    .on('data', function (data) {
-      console.log(data);
-    })
-    .on('finish', function () {
-      console.log('finished')
-    })
-    .on('error', function (err) {
-      console.log(err)
-    });  
+const database = {
+  user: 'root',
+  password: 'khoa0319',
+  address: 'localhost',
+  file: './models/socialhub.sql'
 }
+
+let mysqlImport = spawn('mysql', [
+  '-u' + database.user,
+  '-p' + database.password,
+  '-h' + database.address]);
+mysqlImport.stdin.write('source ' + database.file);
+mysqlImport.stdin.end();
+
+mysqlImport.stdout
+  .on('data', function (data) {
+    console.log(data);
+  })
+  .on('finish', function () {
+    console.log('finished')
+  })
+  .on('error', function (err) {
+    console.log(err)
+  });  
