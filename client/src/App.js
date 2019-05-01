57f136330a63919d620a7133cea6fb9444370765
khoa0319 <khoa0319@gmail.com>
2019-05-01 13:03:12 +0700

    install npm dependencies and format app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar'
 import Loginpage from './components/loginpage/loginpage';
-import {BrowserRouter} from 'react-router-dom';
-import {Route,Switch} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Profile from './components/profilepage/Profile';
 import Activity from './components/model/Activity';
 import Noti from './components/model/Noti';
@@ -13,22 +13,22 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-      <Navbar/>
-      <div className="container app-content mt-10">
-      <Switch>
-      <Route path="/" exact component={Loginpage}/>
-      <Route path="/profile"  component={Profile}/> 
-      <Route component={NoMatch}/> 
-      </Switch>
-      {/* <Route path="/profile" exact component={Profile}/>  
+        <div className="App">
+          <Navbar />
+          <div className="container app-content mt-10">
+            <Switch>
+              <Route path="/" exact component={Loginpage} />
+              <Route path="/profile" component={Profile} />
+              <Route component={NoMatch} />
+            </Switch>
+            {/* <Route path="/profile" exact component={Profile}/>  
       <Route path="/admin" exact component={Table}/> */}
-      </div>
-      </div>
-      <Activity/>
-      <Noti/>
+          </div>
+        </div>
+        <Activity />
+        <Noti />
       </BrowserRouter>
-      
+
     );
   }
 }
